3bb551e8363876a1f2bc8e9ca98182eaf3c4ad6d
Ella Crabtree <43425972+EllaCrabtree@users.noreply.github.com>
2023-04-11 22:19:33 -0400

    Delete projectsProjects.cy.jsx

--- frontend/pages/projectsProjects.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Projects from './projects'
-
-describe('<Projects />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Projects />)
-  })
-})
