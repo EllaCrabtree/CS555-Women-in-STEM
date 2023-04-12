5a881d14cff9cc57773aceecb5e636afb705f914
Ella Crabtree <43425972+EllaCrabtree@users.noreply.github.com>
2023-04-11 22:16:54 -0400

    Delete indexProject.cy.jsx

--- frontend/pages/project/[projectId]/indexProject.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Project from './index'
-
-describe('<Project />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Project />)
-  })
-})
