561896ba51d1c937adc07b7a7d256e0fcfac532d
Ella Crabtree <43425972+EllaCrabtree@users.noreply.github.com>
2023-04-11 22:18:47 -0400

    Delete reminderReminder.cy.jsx

--- frontend/pages/reminderReminder.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Reminder from './reminder'
-
-describe('<Reminder />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Reminder />)
-  })
-})
