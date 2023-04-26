e8d7621ebadd12adfc52990c5b6289acfbc85b54
Ari <ari@ceiphr.com>
2023-04-25 22:13:47 -0400

    Fixing chat. WIP.

---
+++ frontend/workers/chat.js
@@ -0,0 +1,19 @@
+async function handleRequest(request) {
+    const upgradeHeader = request.headers.get("Upgrade");
+    if (!upgradeHeader || upgradeHeader !== "websocket") {
+        return new Response("Expected Upgrade: websocket", { status: 426 });
+    }
+
+    const webSocketPair = new WebSocketPair();
+    const [client, server] = Object.values(webSocketPair);
+
+    server.accept();
+    server.addEventListener("message", (event) => {
+        console.log(event.data);
+    });
+
+    return new Response(null, {
+        status: 101,
+        webSocket: client,
+    });
+}
