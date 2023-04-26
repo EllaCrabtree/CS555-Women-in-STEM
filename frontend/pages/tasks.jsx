9cd7a6e7c86e0de75eb0ce92564f8da18d14b26c
facundorechter <facundorechter@gmail.com>
2023-04-25 20:23:55 -0400

    More translation

--- frontend/pages/tasks.jsx
+++ frontend/pages/tasks.jsx
@@ -7,27 +7,44 @@ import Header from "@components/Header";
 import Footer from "@components/Footer";
 
 export default function Dashboard() {
-    // Check if the user isn't logged in
-    // If the user isn't logged in, redirect to the home page
-    const auth = useAuth();
-    useEffect(() => {
-        if (!auth.authUser) {
-            Router.push("/");
-        }
-    }, [auth]);
+	// Check if the user isn't logged in
+	// If the user isn't logged in, redirect to the home page
+	const auth = useAuth();
+	useEffect(() => {
+		if (!auth.authUser) {
+			Router.push("/");
+		}
+	}, [auth]);
 
-    return (
-        <div className="whitePageWrapper">
-            <Header type="header" />
-            <main className="content">
-                <div className="searchbar">
-                    <img src="/search.png" alt="searchpic" />
-                    <input type="text" placeholder="Search for Tasks..." />
-                </div>
-                <h1>All Tasks</h1>
-                <div>No tasks available.</div>
-            </main>
-            <Footer type="footer" />
-        </div>
-    );
+	if (!spanish) {
+		return (
+			<div className="whitePageWrapper">
+				<Header type="header" />
+				<main className="content">
+					<div class="searchbar">
+						<img src="/search.png" alt="searchpic" />
+						<input type="text" placeholder="Search for Tasks..." />
+					</div>
+					<h1>All Tasks</h1>
+					<div>No tasks available.</div>
+				</main>
+				<Footer type="footer" />
+			</div>
+		);
+	} else {
+		return (
+			<div className="whitePageWrapper">
+				<Header type="header" />
+				<main className="content">
+					<div class="searchbar">
+						<img src="/search.png" alt="searchpic" />
+						<input type="text" placeholder="Buscar tareas..." />
+					</div>
+					<h1>Todas las Tareas</h1>
+					<div>No hay tareas disponibles.</div>
+				</main>
+				<Footer type="footer" />
+			</div>
+		);
+	}
 }
